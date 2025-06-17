076e2655caaa950b4a25f377c917cdb247532b0e
Pablo Silva <pablosilva.personalcontact@gmail.com>
2025-06-16 21:58:47 -0300

    Conteudo Java aula 05

---
+++ Eclipse/Workspace/n25-java1-01/src/br/com/senaisp/baur/aula05/Exemplo02.java
@@ -0,0 +1,20 @@
+package br.com.senaisp.baur.aula05;
+
+import java.util.Scanner;
+
+public class Exemplo02 {
+
+	public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Digite seu nome: ");
+    String nome = sc.next(); //Utiliza o delimitador padrão
+    System.out.println("Seu nome é " + nome);
+    //Agora pegando o nome completo
+    System.out.println("Digite seu nome completo: ");
+    sc.nextLine(); //Captura oenter do comando anterior
+    String nomeCompleto = sc.nextLine(); // Usa o deimitador \n
+    System.out.println("Nome completo: " + nomeCompleto);
+    sc.close();
+	}
+
+}
